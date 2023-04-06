57480316ef22c174ed2dcc1683355c865ffd56e4
Save2541 <rattanakorn2541@gmail.com>
2023-04-06 13:48:52 -0400

    define epsilon before mu

--- EM1D_PIC/constants.py
+++ EM1D_PIC/constants.py
@@ -2,8 +2,8 @@ import math
 
 # PHYSICAL CONSTANTS (MKS UNITS)
 c = 3.00E8  # speed of light
-mu = 1.26E-6  # vacuum permeability
-epsilon = 1 / mu / c ** 2  # 8.85E-12  # vacuum permittivity
+epsilon = 8.85E-12  # vacuum permittivity
+mu = 1 / epsilon / c ** 2  # vacuum permeability
 me_real = 9.11E-31  # electron mass
 mp_real = 1.67E-27  # proton mass
 qe_real = 1.60E-19  # elementary charge
